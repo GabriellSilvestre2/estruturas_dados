5575710c72bcdea1c591720f357e785c54080522
Gabriel Silvestre <gabriellsilvestrerego@gmail.com>
2024-09-18 01:36:29 -0300

    Delete fille

--- Pilha Estática/Pilha De Histórico/InterfaceDePedidos.kt
+++
@@ -1,9 +0,0 @@
-interface InterfaceDePedidos {
-
-    fun empilharPedidos(pedido: String)
-    fun desempilharPedidos(): String
-    fun estaCheia(): Boolean
-    fun estaVazia(): Boolean
-    fun relatorioDePedidos()
-
-}
