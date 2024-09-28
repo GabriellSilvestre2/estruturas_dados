d2c56ae8512656b339ad719de0eb1ec42139420a
Gabriel Silvestre <gabriellsilvestrerego@gmail.com>
2024-09-28 14:40:39 -0300

    Listavel

--- Lista Estática/Listavel.kt
+++ Lista Estática/Listavel.kt
@@ -1,1 +1,19 @@
-
+interface Listavel {
+    // PRINCIPAIS
+    fun inserir(posicao: Int, dado: Any?)
+    fun anexar(dado: Any?)
+    fun selecionar(posicao: Int): Any?
+    fun selecionarTodos(): Array<Any?>
+    fun atualizar(posicao: Int, dado: Any?)
+    fun apagar(posicao: Int): Any?
+    fun limpar(): Boolean
+    // AUXILIARES
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
+}
+/*
+inserirInicio(dado)
+selecionarInicio(): Any
+atualizarFim(dado)
+*/
